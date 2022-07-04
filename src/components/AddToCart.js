a931bac59cf956292710ff461a194f169f86958a
Rehber Moin <docflex@Rehbers-MacBook-Pro.local>
2022-07-04 10:26:49 +0530

    Test

--- src/components/AddToCart.js
+++ src/components/AddToCart.js
@@ -6,7 +6,6 @@ import { FaCheck } from "react-icons/fa";
 import { useCartContext } from "../context/cart_context";
 import AmountButtons from "./AmountButtons";
 const AddToCart = ({ product }) => {
-    // add to cart
     const { addToCart } = useCartContext();
     const { id, stock, colors } = product;
     const [mainColor, setMainColor] = useState(colors[0]);
